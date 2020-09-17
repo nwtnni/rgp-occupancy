a78c5bcbfe4e518996ac72b386cdcb14d650752a
Newton Ni <nwtnni@gmail.com>
2020-09-16 23:59:42 -0400

    Implement minimal proof of concept

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::io::Write as _;
+use std::fs;
+use std::thread;
+use std::time;
+
+use once_cell::sync;
+use reqwest::blocking;
+
+static COUNT: sync::Lazy<regex::Regex> = sync::Lazy::new(|| {
+        regex::RegexBuilder::new(r#"'SBO'[^}]*'count'[^,]*(\d+),"#)
+            .multi_line(true)
+            .build()
+            .expect("[INTERNAL ERROR]: invalid regex")
+
+});
+
+static URL: &str = "https://portal.rockgympro.com/portal/public/7a2ec613bb982d4ba91785c2cdb45902/occupancy?&iframeid=occupancyCounter&fId=1325";
+static SLEEP: time::Duration = time::Duration::from_secs(60 * 10);
+
+fn main() -> io::Result<()> {
+
+    let client = blocking::Client::builder()
+        .user_agent("rock-spot-bot/1.0 nwtnni@gmail.com")
+        .build()
+        .expect("[INTERNAL ERROR]: invalid reqwest client");
+
+    let mut log = fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("log.txt")?;
+
+    loop {
+        match client.get(URL).send().and_then(blocking::Response::text) {
+        | Ok(html) => {
+            let count = COUNT
+                .captures(&*html)
+                .and_then(|captures| captures.get(1))
+                .expect("[INTERNAL ERROR]: count CSS selector returned nothing")
+                .as_str();
+
+            let time = time::SystemTime::now()
+                .duration_since(time::UNIX_EPOCH)
+                .expect("[INTERNAL ERROR]: time went backwards");
+
+            writeln!(&mut log, "{},{}", time.as_secs(), count)?;
+        }
+        | Err(error) => eprintln!("Error: {:?}", error),
+        }
+
+        thread::sleep(SLEEP);
+    }
 }
